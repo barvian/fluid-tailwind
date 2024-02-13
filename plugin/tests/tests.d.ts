c7e17889db0012968d30c9241bc9dad42160a2fa
Maxwell Barvian <max@barvian.me>
2024-02-13 15:22:38 -0600

    Random fixes

--- plugin/tests/tests.d.ts
+++ plugin/tests/tests.d.ts
@@ -1,6 +1,6 @@
 declare module 'bun:test' {
   interface AsymmetricMatchers {
-    async toMatchFormattedCss(expected: string): void;
+    toMatchFormattedCss(expected: string): void;
   }
   interface Matchers<R> {
     toMatchFormattedCss(expected: string): R;
