e60df58bbddb43f0ef5b1e8a06e2bf85a6fc55b6
Maxwell Barvian <max@barvian.me>
2024-05-12 17:44:02 -0700

    Add white ring to dark code

--- site/pages/_Split.svelte
+++ site/pages/_Split.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-	class="{cls} grid {cols} {rows} {shadow} auto-rows-fr overflow-clip rounded-xl *:!m-0 [&>.expressive-code+.expressive-code]:[border-top:1px_solid_rgb(54,62,80)] [&>.expressive-code]:!h-full [&>.expressive-code_.frame]:!h-full [&>.expressive-code_.frame]:!shadow-none [&>.expressive-code_:is(.frame,pre,.frame_.header)]:![border-radius:0]"
+	class="{cls} grid {cols} {rows} {shadow} relative auto-rows-fr overflow-clip rounded-xl *:!m-0 dark:border-white/10 dark:after:pointer-events-none dark:after:absolute dark:after:inset-0 dark:after:z-10 dark:after:size-full dark:after:rounded-xl dark:after:ring-1 dark:after:ring-inset dark:after:ring-white/10 [&>.expressive-code+.expressive-code]:[border-top:1px_solid_rgb(54,62,80)] [&>.expressive-code]:!h-full [&>.expressive-code_.frame]:!h-full [&>.expressive-code_.frame]:!shadow-none [&>.expressive-code_.frame]:after:!hidden [&>.expressive-code_:is(.frame,pre,.frame_.header)]:![border-radius:0]"
 >
 	<slot />
 </div>
