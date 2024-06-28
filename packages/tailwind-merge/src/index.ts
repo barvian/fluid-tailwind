ce66d30d3381a48149eb70bc72e725c660fca116
Maxwell Barvian <max@barvian.me>
2024-06-28 18:12:04 -0500

    Improve merge algo

--- packages/tailwind-merge/src/index.ts
+++ packages/tailwind-merge/src/index.ts
@@ -1,29 +1,73 @@
 import { mergeConfigs, type Config } from 'tailwind-merge'
 
-const fluidValidator = (value: string) => {
-	const parts = value.split('/')
-	return parts.length >= 2
-}
+type ArrayElement<ArrayType extends readonly unknown[]> =
+	ArrayType extends readonly (infer ElementType)[] ? ElementType : never
 
 export function withFluid(config: Config<string, string>) {
 	if (config.prefix)
 		throw new Error(`@fluid-tailwind/tailwind-merge doesn't work with custom prefixes`)
 
+	type ClassGroupKeys = keyof typeof config.classGroups
+	type ClassGroup = (typeof config.classGroups)[ClassGroupKeys]
+	type ClassDefinition = ArrayElement<ClassGroup>
+	type ClassObject = Exclude<ClassDefinition, string | Function>
+	interface ThemeGetter {
+		(theme: (typeof config)['theme']): ClassGroup
+		isThemeGetter: true
+	}
+	// TS needs the explicit predicate here:
+	const isThemeGetter = (def: ClassDefinition): def is ThemeGetter =>
+		typeof def === 'function' && 'isThemeGetter' in def && def.isThemeGetter
+	type ClassValidator = (val: string) => boolean
+
+	const buildFluidValidator =
+		(defs: Exclude<ClassDefinition, ClassObject | ThemeGetter>[]): ClassValidator =>
+		(val) => {
+			// TODO: this won't handle values with `/` in them
+			const parts = val.split('/')
+			return (
+				parts.length === 2 &&
+				defs.length > 0 &&
+				parts.every((v) => defs.some((d) => (typeof d === 'string' ? d === v : d(v))))
+			)
+		}
+
+	const resolveThemeGetters = (group: ClassGroup): Exclude<ClassDefinition, ThemeGetter>[] =>
+		group.flatMap((def) => (isThemeGetter(def) ? resolveThemeGetters(def(config.theme)) : [def]))
+
+	const fluidizeClassGroup = (
+		group: ClassGroup,
+		toplevel = false
+	): [fluidValidator: ClassValidator, ...ClassObject[]] => {
+		// Separate class objects from other defs:
+		const nonObjDefs: Exclude<ClassDefinition, ThemeGetter | ClassObject>[] = []
+		const objDefs: ClassObject[] = []
+
+		// Resolve theme getters first
+		resolveThemeGetters(group).forEach((def) => {
+			if (typeof def !== 'object') return !toplevel && nonObjDefs.push(def)
+
+			objDefs.push(
+				Object.fromEntries(
+					Object.entries(def).flatMap(([name, defs]) => {
+						const newDefs = fluidizeClassGroup(defs)
+						return toplevel
+							? [
+									[`~${name}`, newDefs],
+									[`~-${name}`, newDefs]
+								]
+							: [[name, newDefs]]
+					})
+				)
+			)
+		})
+
+		return [buildFluidValidator(nonObjDefs), ...objDefs]
+	}
+
 	const classGroups = Object.fromEntries(
 		Object.entries(config.classGroups).map(([name, group]) => {
-			return [
-				name,
-				group
-					.filter((def) => typeof def === 'object')
-					.map((obj) =>
-						Object.fromEntries(
-							Object.entries(obj).flatMap(([name, defs]) => [
-								[`~${name}`, [fluidValidator]],
-								[`~-${name}`, [fluidValidator]]
-							])
-						)
-					)
-			]
+			return [name, fluidizeClassGroup(group, true).slice(1)]
 		})
 	)
 
--- packages/tailwind-merge/tests/index.test.ts
+++ packages/tailwind-merge/tests/index.test.ts
@@ -10,12 +10,23 @@ it('errors if the prefix is set', () => {
 	}).toThrow()
 })
 
-it(`doesn't add static utilities`, () => {
-	expect(twMerge('relative ~relative')).toBe('relative ~relative')
-})
-
 it('validates fluid utilities correctly', () => {
+	expect(twMerge('relative ~relative')).toBe('relative ~relative')
 	expect(twMerge('m-[2px] ~m-[2px]')).toBe('m-[2px] ~m-[2px]')
+	expect(twMerge('m-[2px] ~m-[2px]/_nonexistent')).toBe('m-[2px] ~m-[2px]/_nonexistent')
+	expect(twMerge('m-[2px] ~m-[2px]/auto')).toBe('~m-[2px]/auto')
+	const twMergeWithTheme = extendTailwindMerge<'test'>(
+		{
+			extend: {
+				theme: {
+					spacing: ['big']
+				}
+			}
+		},
+		withFluid
+	)
+	expect(twMergeWithTheme('m-[2px] ~m-[2px]/big')).toBe('~m-[2px]/big')
+	expect(twMergeWithTheme('m-[2px] ~m-[2px]/_nonexistent')).toBe('m-[2px] ~m-[2px]/_nonexistent')
 })
 
 it(`handles custom class groups`, () => {
@@ -36,7 +47,7 @@ it(`handles custom class groups`, () => {
 	expect(twMerge('test-[2px] ~test-[2px]/[3px]')).toBe('~test-[2px]/[3px]')
 })
 
-it(`doesn't handles static class groups`, () => {
+it(`doesn't handle static class groups`, () => {
 	const twMerge = extendTailwindMerge<'test'>(
 		{
 			extend: {
@@ -56,4 +67,14 @@ it('handles arbitrary length conflicts correctly', () => {
 
 it('handles negative fluid utilities', () => {
 	expect(twMerge('m-[2px] ~-m-[2px]/[3px]')).toBe('~-m-[2px]/[3px]')
+})
+
+// https://github.com/barvian/fluid-tailwind/discussions/29
+it('works with variants', () => {
+	expect(twMerge('~text-base/lg sm:~sm:~text-lg/xl ~text-2xl/3xl sm:~sm:~text-3xl/4xl')).toBe(
+		'~text-2xl/3xl sm:~sm:~text-3xl/4xl'
+	)
+	expect(twMerge('text-base sm:text-lg ~text-2xl/3xl sm:~sm:~text-3xl/4xl')).toBe(
+		'sm:text-lg ~text-2xl/3xl sm:~sm:~text-3xl/4xl' // TODO: ideally this should be '~text-2xl/3xl sm:~sm:~text-3xl/4xl'
+	)
 })
